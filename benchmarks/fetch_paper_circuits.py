d5afffd45936e9889efa47eb512c8e90ee2bd2c7
Thiri Yamin Hsu <ThiriYaminHsu@my.uopeople.edu>
2026-08-14 18:39:48 +0100

    Create fetch_paper_circuits.py for benchmark downloads
    Add a script to download benchmark circuits for evaluation.

---
+++ benchmarks/fetch_paper_circuits.py
@@ -0,0 +1,9 @@
+"""Download the paper's benchmark circuits for evaluation.
+
+TODO: migrate the benchmark-loading code from Section 11 of the dissertation notebook,
+which pulls the Original/ QASM/qc files from:
+https://github.com/qilimanjaro-tech/Circopt-RL-ZXCalc/tree/main/rl-zx/results/circuits/Original
+
+Downloaded files should land in benchmarks/Original/ (gitignored -- re-run this script
+rather than committing the circuit files).
+"""
