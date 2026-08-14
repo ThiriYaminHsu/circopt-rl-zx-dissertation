bffedfdd97a6f0485f6f5f570993be727a5afd0f
Thiri Yamin Hsu <ThiriYaminHsu@my.uopeople.edu>
2026-08-14 18:30:10 +0100

    Create generators.py with initial documentation
    Add initial comments and TODOs for circuit generators.

---
+++ src/circopt_adder/generators.py
@@ -0,0 +1,7 @@
+"""Circuit generators injected into the Gym environment at construction time.
+
+TODO: migrate from Section 4 of the dissertation notebook:
+    - make_random_circuit_generator   (replicates Riu et al.'s training regime)
+    - ripple_carry_adder, make_adder_generator, make_fixed_adder_generator
+      (new: not in the original repo -- this project's random-vs-structured ablation)
+"""
