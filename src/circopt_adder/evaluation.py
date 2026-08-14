74757bcbf383521dd3e1aecf0cd4d1df94d40cf6
Thiri Yamin Hsu <ThiriYaminHsu@my.uopeople.edu>
2026-08-14 18:33:37 +0100

    Create evaluation.py with initial documentation
    Add evaluation protocol documentation and TODOs for migration.

---
+++ src/circopt_adder/evaluation.py
@@ -0,0 +1,5 @@
+"""Evaluation protocol: run trained agents and baselines on held-out circuits.
+
+TODO: migrate run_agent_eval and evaluate_all_methods from Section 11 of the
+dissertation notebook.
+"""
